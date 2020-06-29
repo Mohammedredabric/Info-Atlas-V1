7630d48ae87cac4fc417281c7fa0ab6e6dd27813
Mohammed reda Bric <mohammedridabric@gmail.com>
2020-06-29 04:37:57 +0200

    Notification Of Listing Created/Published

---
+++ app/Notifications/ListPublished.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ListPublished extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public $list;
+    public function __construct($list)
+    {
+        $this->list =$list;
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $subject = sprintf('%s: the directory list Has been Published  %s!', config('app.name'), 'Mohammed rida bric ');
+        $greeting = sprintf('DIRECTORY LIST %s!', $notifiable->name);
+
+        return (new MailMessage)
+            ->subject($subject)
+            ->greeting($greeting)
+            ->line(' ATLAS INFO BUSINESS DIRECTORY LISTING ')
+            ->action('Check Your Listing', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
